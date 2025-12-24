aecdb6cbd53d80148f7def1801ecd0b9b19c667f
Malak Ahmed Samih <164598112+Malak-A7med@users.noreply.github.com>
2025-12-25 01:13:15 +0200

    Delete computer_play_gui.py

--- computer_play_gui.py
+++
@@ -1,6 +0,0 @@
-import math
-def computer_play_gui(self, state):
-    _, best_move = self.alphabeta(state, 4, -math.inf, math.inf)
-    if best_move:
-        return self.take_action(state, best_move)
-    return state
